21012023588e494816b482e2701ed25756a2659a
joy-amorin <amorinkaren2021@gmail.com>
2025-08-23 21:29:42 -0300

    add free resources data file

---
+++ my-personal-web/src/data/resources.js
@@ -0,0 +1,9 @@
+export const freeResources = [
+{
+    id: 'guide001',
+    title: 'Mini guía de práctica musical', 
+    description: 'Guía rápida para tu practica musical autodidacta',
+    image: 'https://web-resources.joy-resources.workers.dev/ebooks/mini-guia-de-practica-musical.jpg',
+
+}
+]
