4ebeaa99b059f94f16349803cb512962984db850
joy-amorin <amorinkaren2021@gmail.com>
2025-08-30 18:16:01 -0300

    change the order of the sections in Navbar

--- my-personal-web/src/app/components/Navbar.js
+++ my-personal-web/src/app/components/Navbar.js
@@ -61,15 +61,7 @@ export default function Navbar() {
               Blog
             </a>
           </li>
-          <li>
-            <Link
-              href="/tienda"
-              className="relative after:block after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
-            
-            >
-              Tienda
-            </Link>
-          </li>
+         
           {/* Barra desplegable de recursos */}
           <li className="relative">
             <button
@@ -95,6 +87,15 @@ export default function Navbar() {
                 </motion.ul>
               )}
             </AnimatePresence>
+          </li>
+           <li>
+            <Link
+              href="/tienda"
+              className="relative after:block after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full"
+            
+            >
+              Tienda
+            </Link>
           </li>
           <li>
             <a
